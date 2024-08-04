2f9bc35ae1874c01115829db6bfd58b9366b9403
Vikri Permana <vikripermana91@gmail.com>
2024-08-04 15:50:50 +0700

    :lipstick: Responsive at 1366px

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -27,16 +27,16 @@ const Layout = ({ children }) => {
       </m.div>
       <div className="fixed w-full flex justify-between py-5 px-10 border-b-2 border-b-primary text-primary bg-secondary z-40">
         <div className="flex items-center gap-3">
-          <img src={Logo} />
-          <span className="w-1 h-full bg-primary" />
-          <p className="font-semibold">Bakmie Ayam 58</p>
+          <img src={Logo} className="w-10 xl:w-auto" />
+          <span className="w-0.5 xl:w-1 h-full bg-primary" />
+          <p className="font-semibold text-xs xl:text-base">Bakmie Ayam 58</p>
         </div>
         <div className="flex gap-10 items-center">
           {routes.map((item, key) => (
             <div key={key}>
               <Link
                 to={item.path}
-                className="px-2 py-1 rounded-full hover:bg-[#96E5A0] transition-all"
+                className="px-2 py-1 rounded-full hover:bg-[#96E5A0] text-xs xl:text-base transition-all"
               >
                 {item.name}
               </Link>
@@ -50,27 +50,31 @@ const Layout = ({ children }) => {
           <p className="font-title font-semibold text-2xl uppercase">
             Bakmie Ayam 58
           </p>
-          <div className="grid grid-cols-2 w-1/2">
-            <div className="flex gap-2 items-center text-sm">
+          <div className="grid grid-cols-2 w-[60%] gap-4">
+            <div className="flex gap-2 items-center text-xs xl:text-sm">
               <IoLocationSharp />
               <p>Teuku Umar No.4/60</p>
             </div>
-            <div className="flex gap-2 items-center text-sm">
+            <div className="flex gap-2 items-center text-xs xl:text-sm">
               <FaInstagram />
+              <Link to={"https://www.instagram.com/bakmieayam58?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="} target="_blank">
               <p>@bakmieayam58</p>
+              </Link>
             </div>
-            <div className="flex gap-2 items-center text-sm">
+            <div className="flex gap-2 items-center text-xs xl:text-sm">
               <IoLocationSharp />
               <p>Dalem Kaum No.114</p>
             </div>
-            <div className="flex gap-2 items-center text-sm">
+            <div className="flex gap-2 items-center text-xs xl:text-sm">
               <FaWhatsapp />
+              <Link to={"https://wa.me/6282129679572"} target="_blank">
               <p>082129679572</p>
+              </Link>
             </div>
           </div>
         </div>
         <div className="flex items-end justify-end">
-          <p className="font-semibold">© 2024 BAKMIE AYAM 58</p>
+          <p className="font-semibold text-xs xl:text-base">© 2024 BAKMIE AYAM 58</p>
         </div>
       </div>
     </div>
--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -2,19 +2,19 @@ import Layout from "src/components/Layout";
 import CircleText from "src/assets/circle-name.svg";
 import MieKalong from "src/assets/mie-kalong.svg";
 import MiePutih from "src/assets/mie-putih.svg";
-import RicaRica from "src/assets/rica-rica.svg";
-import Jamur from "src/assets/jamur.svg";
+import Jamur from "src/assets/rica-rica.svg";
+import RicaRica from "src/assets/jamur.svg";
 import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import { useEffect, useRef, useState } from "react";
 import { IoPlayCircle } from "react-icons/io5";
-import {motion as m, stagger, useAnimate} from "framer-motion"
+import { motion as m, stagger, useAnimate } from "framer-motion";
 
 const Menu = () => {
   const ref = useRef();
-  const [selected,setSelected] = useState('kalong')
+  const [selected, setSelected] = useState("kalong");
 
-  const [scope,animate] = useAnimate()
+  const [scope, animate] = useAnimate();
 
   useEffect(() => {
     animate(
@@ -26,22 +26,23 @@ const Menu = () => {
 
   return (
     <Layout>
-      <div className="py-32 px-10 overflow-hidden">
+      <div className="py-20 xl:py-32 px-10 overflow-hidden">
         <div className="flex w-full gap-28">
           <div className="flex justify-end relative pt-10 min-w-[45%] max-w-[45%] z-20">
             <m.img
-            initial={{opacity:0}}
-            animate={{opacity:1}}
-            transition={{delay:4.5,duration:2}}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 4.5, duration: 2 }}
               src={CircleText}
               loading="lazy"
-              className="animate-[spin_20s_linear_infinite] absolute"
+              className="animate-[spin_20s_linear_infinite] absolute right-5 top-12 w-[26rem] xl:w-auto xl:top-auto xl:right-auto"
             />
-            <m.div 
-            initial={{opacity:0}}
-            animate={{opacity:1}}
-            transition={{delay:2,duration:0.3}}
-            className="h-full w-full ">
+            <m.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 2, duration: 0.3 }}
+              className="h-full w-full "
+            >
               <Swiper
                 modules={[Pagination]}
                 spaceBetween={50}
@@ -50,61 +51,75 @@ const Menu = () => {
                 loop
               >
                 <SwiperSlide>
-                  <SlideComponent src={MieKalong} name="kalong" btnRef={ref} onChange={setSelected} />
+                  <SlideComponent
+                    src={MieKalong}
+                    name="kalong"
+                    btnRef={ref}
+                    onChange={setSelected}
+                  />
                 </SwiperSlide>
                 <SwiperSlide>
-                  <SlideComponent src={MiePutih} name="putih" btnRef={ref} onChange={setSelected} />
+                  <SlideComponent
+                    src={MiePutih}
+                    name="putih"
+                    btnRef={ref}
+                    onChange={setSelected}
+                  />
                 </SwiperSlide>
               </Swiper>
             </m.div>
           </div>
           <div className="flex-1 relative z-0">
-            <m.div 
-            initial={{height:0}}
-            animate={{height:"100%"}}
-            transition={{duration:2,ease:"easeInOut"}}
-            className="absolute w-full h-full bg-primary z-0 -bottom-72 2xl:scale-x-[2] scale-x-[2.5]">
+            <m.div
+              initial={{ height: 0 }}
+              animate={{ height: "100%" }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              className="absolute w-full h-full bg-primary z-0 -bottom-72 2xl:scale-x-[2] scale-x-[2.5]"
+            >
               <div className="relative w-full h-full">
                 <div className="wave"></div>
               </div>
             </m.div>
             <div className="z-20 flex flex-col justify-between w-full h-full">
-              <m.p 
-              initial={{opacity:0}}
-              animate={{opacity:1}}
-              transition={{delay:2,duration:0.3}}
-              className="font-title text-7xl text-primary font-bold mt-20">
+              <m.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 2, duration: 0.3 }}
+                className="font-title text-6xl xl:text-7xl text-primary font-bold mt-20"
+              >
                 Menu Andalan <br /> Bakmi Ayam 58
               </m.p>
-              <div ref={scope} className="z-20 flex flex-col gap-10 pt-24">
-                <p className="font-semibold font-title text-5xl text-secondary">
-                  {selected === "kalong" ? 'BAKMIE KALONG' : 'BAKMIE ORIGINAL'}
+              <div ref={scope} className="z-20 flex flex-col gap-3 xl:gap-10 pt-36 xl:pt-24">
+                <p className="font-semibold font-title text-3xl xl:text-5xl text-secondary">
+                  {selected === "kalong" ? "BAKMIE KALONG" : "BAKMIE ORIGINAL"}
                 </p>
-                <p className="font-semibold font-title text-5xl text-secondary">
+                <p className="font-semibold font-title text-3xl xl:text-5xl text-secondary">
                   Isi :
                 </p>
-                <p className="text-secondary text-xl">
-                {selected === "kalong" ? 'Mie Hitam (asin atau manis), sayur, kuah.' : 'Mie Kuning (asin atau manis), sayur, kuah.'}
+                <p className="text-secondary text-sm xl:text-xl">
+                  {selected === "kalong"
+                    ? "Mie Hitam (asin atau manis), sayur, kuah."
+                    : "Mie Kuning (asin atau manis), sayur, kuah."}
                 </p>
                 <div className="flex">
                   <div className="flex flex-col flex-1 gap-10">
-                    <p className="font-title text-secondary text-5xl font-semibold">
+                    <p className="font-title text-secondary text-3xl xl:text-5xl font-semibold">
                       Topping :
                     </p>
                     <div className="flex gap-10 text-secondary">
                       <div className="flex flex-col items-center gap-5">
-                        <img src={Jamur} loading="lazy" />
+                        <img src={Jamur} loading="lazy" className="w-28 xl:w-auto" />
                         <p>Ayam Jamur</p>
                       </div>
                       <div className="flex flex-col items-center gap-5">
-                        <img src={RicaRica} loading="lazy" />
+                        <img src={RicaRica} loading="lazy" className="w-28 xl:w-auto" />
                         <p>Ayam Rica Rica</p>
                       </div>
                     </div>
                   </div>
                   <div>
                     <button onClick={() => ref.current.click()}>
-                      <IoPlayCircle className="text-5xl text-white" />
+                      <IoPlayCircle className="text-3xl xl:text-5xl text-white" />
                     </button>
                   </div>
                 </div>
@@ -118,21 +133,18 @@ const Menu = () => {
 };
 
 // eslint-disable-next-line react/prop-types
-const SlideComponent = ({ src, name, btnRef,onChange }) => {
+const SlideComponent = ({ src, name, btnRef, onChange }) => {
   const swiper = useSwiper();
   const swiperSlide = useSwiperSlide();
 
   if (swiperSlide.isActive) {
-    onChange(name)
+    onChange(name);
   }
 
   return (
     <div className="flex justify-end px-9 pt-5">
-      <button
-        ref={btnRef}
-        onClick={() => swiper.slideNext()}
-      ></button>
-      <img src={src} loading="lazy" />
+      <button ref={btnRef} onClick={() => swiper.slideNext()}></button>
+      <img src={src} loading="lazy" className="w-96 xl:w-auto" />
     </div>
   );
 };
